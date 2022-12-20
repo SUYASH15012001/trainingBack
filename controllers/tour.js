de53eea894493d9d1bea660b29bbb666199f3c9e
SUYASH15012001 <sakshams1501@gmail.com>
2022-12-20 21:11:45 +0530

    token tour creation middleware

--- controllers/tour.js
+++ controllers/tour.js
@@ -2,14 +2,17 @@ import TourModal from "../models/tour.js";
 
 export const createTour = async (req, res) => {
   const tour = req.body;
+  console.log(req.body);
   const newTour = new TourModal({
     ...tour,
+    creator: req.userId,
     createdAt: new Date().toISOString(),
   });
   try {
     await newTour.save();
     res.status(200).json(newTour);
   } catch (err) {
+    console.log(err);
     res.status(400).json({ message: "Something went wrong!." });
   }
 };
